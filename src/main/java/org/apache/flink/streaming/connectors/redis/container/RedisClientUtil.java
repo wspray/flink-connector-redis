3dd527af1d063a9429bfb56760bf73a883004bb5
WangQiang <wangqiang02@yto.net.cn>
2025-09-28 13:10:36 +0800

    fixed

--- src/main/java/org/apache/flink/streaming/connectors/redis/container/RedisClientUtil.java
+++ src/main/java/org/apache/flink/streaming/connectors/redis/container/RedisClientUtil.java
@@ -1,6 +1,5 @@
 package org.apache.flink.streaming.connectors.redis.container;
 
-import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.Validate;
 import org.apache.flink.streaming.connectors.redis.config.FlinkClusterConfig;
 import org.slf4j.Logger;
@@ -12,16 +11,12 @@ public class RedisClientUtil {
 
     private static final Logger LOG = LoggerFactory.getLogger(RedisClientUtil.class);
 
-    private static final int MAX_KEY_LENGTH = 200;
-    private static final int MAX_PASSWORD_LENGTH = 500;
-    private static final int MAX_ENDPOINT_SIZE = 5_000;
-
     public static <T> T executeWithRedisContainer(String endpoints,
                                                   String password,
                                                   String key,
                                                   RedisFunction<T> function) {
-        validateInput(endpoints, password, key);
-
+        Validate.notBlank(endpoints, "endpoints cant be blank");
+        Validate.notBlank(key, "key cant be blank");
         RedisCommandsContainer redisCommandsContainer = null;
         try {
             redisCommandsContainer = RedisCommandsContainerBuilder.build(new FlinkClusterConfig.Builder()
@@ -39,31 +34,6 @@ public class RedisClientUtil {
             throw new RuntimeException("Redis operation failed", e);
         } finally {
             shutdown(redisCommandsContainer);
-        }
-    }
-
-    private static void validateInput(String endpoints, String password, String key) {
-        Validate.notBlank(endpoints, "endpoints cant be blank");
-        Validate.notBlank(key, "key cant be blank");
-
-        if (endpoints.length() > MAX_ENDPOINT_SIZE) {
-            throw new IllegalArgumentException("endpoints too long");
-        }
-        if (StringUtils.isNotEmpty(password) && password.length() > MAX_PASSWORD_LENGTH) {
-            throw new IllegalArgumentException("password too long");
-        }
-        if (key.length() > MAX_KEY_LENGTH) {
-            throw new IllegalArgumentException("key too long");
-        }
-        // 白名单校验 key 是否恶意注入“host:port,host2:port2”以外的字符（如空格、换行、@、# 等）
-        if (!key.matches("[\\w\\-\\.:@*]+")) {
-            throw new IllegalArgumentException("key contains illegal char");
-        }
-        // 简单校验 endpoints 格式
-        for (String node : endpoints.split(",")) {
-            if (!node.matches("^[-\\w\\.]+:\\d{1,5}$")) {
-                throw new IllegalArgumentException("invalid node format: " + node);
-            }
         }
     }
 
--- src/main/java/org/apache/flink/streaming/connectors/redis/container/RedisFunction.java
+++ src/main/java/org/apache/flink/streaming/connectors/redis/container/RedisFunction.java
@@ -2,5 +2,7 @@ package org.apache.flink.streaming.connectors.redis.container;
 
 @FunctionalInterface
 public interface RedisFunction<T> {
+
     T apply(RedisCommandsContainer container) throws Exception;
+
 }
